20f7496a9bf062d2624ded2ae4151bd5b2f8a5d9
Nicolás Andrés Plaza Iñiguez <74793371+niplinig@users.noreply.github.com>
2022-08-06 16:30:20 -0500

    Update script-web-developer-cordova.sh

--- script-web-developer-cordova.sh
+++ script-web-developer-cordova.sh
@@ -10,4 +10,12 @@ mkdir cmdline-tools &&
 mv latest cmdline-tools/latest &&
 cd cmdline-tools/latest/bin &&
 ./sdkmanager --install "platform-tools" "platforms;android-30" "system-images;android-30;google_apis;x86_64" "build-tools;30.0.3" &&
- echo 'script finished the work'
+cd /opt &&
+mkdir gradle &&
+cd gradle &&
+wget https://services.gradle.org/distributions/gradle-7.5.1-bin.zip &&
+unzip gradle-7.5.1-bin.zip &&
+rm gradle-7.5.1-bin.zip &&
+cd gradle-7.5.1/bin &&
+./gradle &&
+export PATH=$PATH:/opt/gradle/gradle-7.5.1/bin
