018a989cc31949c35a461555e5cb2cc690d83c0f
Nicolás Andrés Plaza Iñiguez <74793371+niplinig@users.noreply.github.com>
2022-08-10 21:02:15 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 dnf clean all &&
 free &&
-# echo -e '# Added for speed\nfastestmirror=True\nmax_parallel_downloads=10\ndefaultyes=True\nkeepcache=True' >> /etc/dnf/dnf.conf &&
-#sed -i '7 i GRUB_CMDLINE_LINUX="rhgb quiet mitigations=off"' /etc/default/grub &&
-#echo 'GRUB_CMDLINE_LINUX="zswap.enabled=1 rhgb quiet mitigations=off"' >> /etc/default/grub &&
+echo -e '# Added for speed\nfastestmirror=True\nmax_parallel_downloads=10\ndefaultyes=True\nkeepcache=True' >> /etc/dnf/dnf.conf &&
+sed -i '7 i GRUB_CMDLINE_LINUX="rhgb quiet mitigations=off"' /etc/default/grub &&
+echo 'GRUB_CMDLINE_LINUX="zswap.enabled=1 rhgb quiet mitigations=off"' >> /etc/default/grub &&
 
-dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm gnome-tweaks -y &&
+dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm gnome-tweaks google-chrome-stable -y &&
 
 dnf groupupdate core -y &&
 dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y &&
@@ -15,9 +15,13 @@ dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 
 dnf group upgrade --with-optional Multimedia -y &&
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo &&
-flatpak install flathub com.mattjakeman.ExtensionManager org.gnome.Epiphany -y &&
+flatpak install flathub com.mattjakeman.ExtensionManager org.gnome.Epiphany io.gitlab.librewolf-community org.gustavoperedo.FontDownloader -y &&
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo' &&
+dnf check-update &&
+dnf install code -y &&
+dnf module install nodejs:16 -y &&
 dnf update --refresh -y &&
 dnf clean all &&
 free &&
 reboot
-
